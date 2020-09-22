74a0b2bd6f76e4d0f950c540476a844dffc9d789
Roni Cleber <ronycleber_pm@hotmail.com>
2020-09-22 20:05:38 -0300

    HTML da estrutura da Urna pronta

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        canditados: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABDC',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador'}
+                 ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'AQPT',
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador'}
+                 ]
+            }
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        canditados: [
+            {
+                numero: 99,
+                nome: 'Feltrano da Casa',
+                partido: 'AQPT',
+                vice: 'Joana'
+                fotos: [
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                 ]
+            },
+            {
+                numero: 84,
+                nome: 'Fulano Dois',
+                partido: 'AQPT',
+                vice: 'Ciclando Dois'
+                fotos: [
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                 ]
+            }
+        ]
+    }
+]
